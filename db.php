098e18509298473bfd203ff945f3892b1b502866
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-16 16:42:28 +0200

    create db file w movies array of objects

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+
+$movies = [
+   $star_wars = new Movie('Star Wars', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure unde deleniti in non minima! Quis?', new Genre('Sci-Fi')),
+   $scarface = new Movie('Scarface', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure unde deleniti in non minima! Quis?', new Genre('Action')),
+   $titanic = new Movie('Titanic', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure unde deleniti in non minima! Quis?', new Genre('Drama')),
+];
