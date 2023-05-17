8f674432f8ad3631b3caba3ba2ecdeaa6d9e9263
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-17 10:07:25 +0200

    add poster path

--- db.php
+++ db.php
@@ -1,7 +1,8 @@
 <?php
 
 $movies = [
-   $star_wars = new Movie('Star Wars', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure unde deleniti in non minima! Quis?', new Genre('Sci-Fi')),
-   $scarface = new Movie('Scarface', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure unde deleniti in non minima! Quis?', new Genre('Action')),
-   $titanic = new Movie('Titanic', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure unde deleniti in non minima! Quis?', new Genre('Drama')),
+   $star_wars = new Movie('Star Wars', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure unde deleniti in non minima! Quis?', 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/3drtqLOARQEuFgEmGRlMlWpV3Eb.jpg', new Genre('Sci-Fi')),
+   $scarface = new Movie('Scarface', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure unde deleniti in non minima! Quis?', 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/xQis0pdrJkT6E4E3Co8TDBytJEM.jpg', new Genre('Action')),
+   $titanic = new Movie('Titanic', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure unde deleniti in non minima! Quis?', 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/u24jqgSB8YTRRGNp3nmoLAdFdkj.jpg', new Genre('Drama')),
+   $training_day = new Movie('Training Day', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure unde deleniti in non minima! Quis?', 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/n9KdLwWL0cXkurwB6clXUE3fRjL.jpg', new Genre('Crime')),
 ];
