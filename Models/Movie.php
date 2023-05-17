d38a651201ef34393d88916b5555088030075783
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-17 10:08:11 +0200

    add element poster

--- Models/Movie.php
+++ Models/Movie.php
@@ -5,12 +5,14 @@ class Movie
 {
    public $name;
    public $description;
+   public $poster;
    public $genre;
 
-   public function __construct(string $name, string $description, Genre $genre)
+   public function __construct(string $name, string $description, string $poster, Genre $genre)
    {
       $this->name = $name;
       $this->description = $description;
+      $this->poster = $poster;
       $this->genre = $genre;
    }
 
